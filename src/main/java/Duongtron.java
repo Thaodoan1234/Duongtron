eafcd6478f2808c3c3ea353cf534dbd68db368c2
Thaodoan1234 <thao.doan1@onemount.com>
2021-08-06 01:44:29 +0700

    Initial commit

---
+++ src/main/java/Duongtron.java
@@ -0,0 +1,13 @@
+public class Duongtron {
+    public static void main(String[] args) {
+        float PI = 3.14F;
+        float r = 2.5F, Chuvi, Dientich;
+
+        Chuvi = 2 * r * PI;
+        Dientich = r * r * PI;
+        System.out.println("Bán kính của đường tròn = " + r);
+        System.out.println("Chu vi = " + Chuvi);
+        System.out.println("Diện tích = " + Dientich);
+
+    }
+}
